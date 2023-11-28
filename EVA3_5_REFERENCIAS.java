694a09279a19425950a8de8c14fe26d272ff1f9b
Leo052005 <143568859+Leo052005@users.noreply.github.com>
2023-11-28 15:29:45 -0600

    Add files via upload

---
+++ EVA3_5_REFERENCIAS.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_5_referencias;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author leo
+ */
+public class EVA3_5_REFERENCIAS {
+
+    public static void main(String[] args) {
+        int[] arreglo = new int[10];
+        System.out.println(arreglo);
+        for (int i  : arreglo){
+            System.out.println(i);
+            
+        }
+        String[]arreCade = new String[10];
+        System.out.println(arreCade);
+        Scanner input = new Scanner(System.in);
+        System.out.println(input);
+        
+    }
+}
